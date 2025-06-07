bf62c33d35981d38da568ddee8ed80c691ce8514
anthonypulupa <anthonypulupa2@gmail.com>
2025-06-07 15:15:32 -0500

    Practica 3

---
+++ src/main/java/ec/edu/espoch/practica3/Practica3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practica3;
+
+/**
+ *
+ * @author anthony
+ */
+public class Practica3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Practica3
+    }
+}
